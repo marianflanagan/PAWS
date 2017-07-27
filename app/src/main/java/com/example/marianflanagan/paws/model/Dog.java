c03ff9f0c9c12c77f6ced830192f45d69920c756
Marian Flanagan <marianflanagan@live.co.uk>
2017-07-27 18:17:17 +0100

    90% of customer functionality working

---
+++ app/src/main/java/com/example/marianflanagan/paws/model/Dog.java
@@ -0,0 +1,52 @@
+package com.example.marianflanagan.paws.model;
+
+/**
+ * Created by marianflanagan on 26/07/2017.
+ */
+
+public class Dog {
+    private String name, breed, age, userId;
+
+    public Dog() {
+
+    }
+
+    public Dog(String name, String breed, String age, String userId) {
+        this.userId = userId;
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
---
+++ app/src/main/java/com/example/marianflanagan/paws/model/User.java
@@ -0,0 +1,44 @@
+package com.example.marianflanagan.paws.model;
+
+/**
+ * Created by marianflanagan on 26/07/2017.
+ */
+
+public class User {
+    String name;
+    String phone;
+    String address;
+
+    public User() {
+    }
+
+    public User(String name, String phone, String address) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
